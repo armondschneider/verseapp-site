140306fc3e8957c3bfde70b58fb426eb0c591bba
armond <mondoloves@icloud.com>
2025-03-26 07:40:52 -0700

    update

---
+++ script.js
@@ -0,0 +1,73 @@
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
+  };
+  
+  const texts = ["Verse", "is", "for", "AI", "poetry"];
+  
+  const morphTime = 8;
+  const cooldownTime = 4;
+  
+  let textIndex = texts.length - 1;
+  let time = new Date();
+  let morph = 0;
+  let cooldown = cooldownTime;
+  
+  elts.textContent = texts[textIndex % texts.length];
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+  
+  function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+  
+    let fraction = morph / morphTime;
+    if (fraction > 1) {
+      cooldown = cooldownTime;
+      fraction = 1;
+    }
+  
+    setMorph(fraction);
+  }
+  
+  function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+  
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+  
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+  }
+  
+  function doCooldown() {
+    morph = 0;
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+  }
+  
+  function animate() {
+    requestAnimationFrame(animate);
+  
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0; // Only increment if we were in cooldown in the previous frame.
+    let dt = (newTime - time) / 100;
+    time = newTime;
+  
+    cooldown -= dt;
+  
+    if (cooldown <= 0) {
+      if (shouldIncrementIndex) {
+        textIndex++;
+      }
+      doMorph();
+    } else {
+      doCooldown();
+    }
+  }
+  
+  animate();
